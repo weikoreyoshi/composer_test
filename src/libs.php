aeb99cf53a90b2467977260ad9487f4b3dd76c30
qinwei <qinwei@etcp.cn>
2019-07-17 17:12:30 +0800

    测试文件

---
+++ src/libs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: littleboy
+ * Date: 2019-07-17
+ * Time: 16:27
+ * https://packagist.laravel-china.org
+ * http://packagist.xesv5.com:8093
+ *
+ */
+
+echo 'composer_test';
---
+++ src/testHelper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: littleboy
+ * Date: 2019-07-17
+ * Time: 17:11
+ */
+
+echo 222;
